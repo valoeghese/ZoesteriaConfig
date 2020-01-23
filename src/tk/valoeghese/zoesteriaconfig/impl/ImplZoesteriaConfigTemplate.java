7d34851c4139d90c3f45d02196763aeb94e2f83a
valoeghese <valoeghesemods@gmail.com>
2020-01-24 12:56:31 +1300

    make more values compatible

--- src/tk/valoeghese/zoesteriaconfig/impl/ImplZoesteriaConfigTemplate.java
+++ src/tk/valoeghese/zoesteriaconfig/impl/ImplZoesteriaConfigTemplate.java
@@ -1,5 +1,6 @@
 package tk.valoeghese.zoesteriaconfig.impl;
 
+import java.util.List;
 import java.util.Map;
 
 import tk.valoeghese.zoesteriaconfig.api.template.ConfigTemplate;
@@ -20,7 +21,11 @@ public final class ImplZoesteriaConfigTemplate implements ConfigTemplate {
 	private void injectDefaultsIfAbsent(Map<String, Object> map, Map<String, Object> defaults) {
 		defaults.forEach((key, value) -> {
 			if (!map.containsKey(key)) {
-				map.put(key, value);
+				if (value instanceof Map || value instanceof List || value instanceof String) {
+					map.put(key, value);
+				} else {
+					map.put(key, String.valueOf(value));
+				}
 			} else if (map.get(key) instanceof Map && defaults.get(key) instanceof Map) {
 				this.injectDefaultsIfAbsent((Map<String, Object>) value, (Map<String, Object>) defaults.get(key));
 			}
